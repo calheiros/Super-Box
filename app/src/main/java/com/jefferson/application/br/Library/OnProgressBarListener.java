22e815ab045ee6c6811c3d33967c16f533897737
Jefferson Calheiros <jo.gj10@gmail.com>
2022-04-07 23:35:35 -0300

    Untracked files issue resolved to fix .gitignore

--- app/src/main/java/com/jefferson/application/br/Library/OnProgressBarListener.java
+++
@@ -1,9 +0,0 @@
-package com.jefferson.application.br.library;
-
-/**
- * Created by lelexxx on 15-4-23.
- */
-public interface OnProgressBarListener {
-
-    void onProgressChange(int current, int max);
-}
