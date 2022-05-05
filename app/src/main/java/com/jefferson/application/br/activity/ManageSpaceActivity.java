11d4e60c49e3d9f4a94f8a87ae0025c38cf24a6a
Jefferson Calheiros <jo.gj10@gmail.com>
2022-05-04 22:30:42 -0300

    gradld fix 1

--- app/src/main/java/com/jefferson/application/br/activity/ManageSpaceActivity.java
+++ app/src/main/java/com/jefferson/application/br/activity/ManageSpaceActivity.java
@@ -3,13 +3,14 @@ package com.jefferson.application.br.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.jefferson.application.br.R;
+import android.widget.Toast;
 
-public class MyManageActivity extends AppCompatActivity {
+public class ManageSpaceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-      
+          Toast.makeText(this, "ACTION: " + getIntent().getAction(), Toast.LENGTH_LONG).show();
     }
 }
